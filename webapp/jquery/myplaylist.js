862818df0b1967d2f2143f20ccc6609c1ccf7feb
Mindaugas Ajauskas <m.ajauskas@hotmail.it>
2016-09-15 11:14:15 +0200

    Add files via upload

---
+++ webapp/jquery/myplaylist.js
@@ -0,0 +1,34 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'music/1.mp3',
+        oga:'music/1.ogg',
+        title:'Love The Way You Lie',
+        artist:'Eminem',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.jpg'
+    },
+    {
+        mp3:'music/1.mp3',
+        oga:'music/1.ogg',
+        title:'qweqw qwe Lie',
+        artist:'Eminem',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.jpg'
+    }
+
+];
