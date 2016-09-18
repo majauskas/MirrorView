385c4a62fd7a5fe32a381b6bf132951a45e40e8f
minde-ags <m.ajauskas@hotmail.it>
2016-09-18 16:20:05 +0200

    zxc

--- webapp/js/gest.js
+++ webapp/js/gest.js
@@ -38,7 +38,7 @@ window.gest = (function (window) {
 	var	settings = {
 		framerate: 5,
 		videoCompressionRate: 4,
-		sensitivity: 80,	//value from 0 to 100 (100 => very sensitive)
+		sensitivity: 98,	//value from 0 to 100 (100 => very sensitive)
 		skinFilter: false,
 		debug: {
 			state: false,
